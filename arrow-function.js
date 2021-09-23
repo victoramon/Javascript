3233c3f0ebf71345a8c23779c71ee07491edd38e
victor.ramonmarcos@gmail.com <victor.ramonmarcos@gmail.com>
2021-09-23 18:18:54 -0500

    Javascript

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+/* Funciones de flecha */
+
+function suma (num1, num2) {
+    return num1 + num2;
+}
+
+const suma1 = (num1, num2) => {
+    return num1 + num2;
+}
+
+document.write(`<p> ${suma1(5, 3)} </p>`);
---
+++ ternario.js
@@ -0,0 +1,12 @@
+/* Operador ternario */
+const edad = 20;
+
+/* if(edad >= 18) {
+    console.log('Eres mayor de edad');
+}else {
+    console.log('Eres menor de edad');
+} */
+
+const mensaje = edad > 18 ? 'Eres mayor de edad' : 'Eres menor de edad';
+
+document.write(`<p> ${mensaje} </p>`);
